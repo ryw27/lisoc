7fe9d73dc29e4d8e7b6582d0070060bd389a516b
ryw27 <rywbook06@gmail.com>
2025-05-09 23:39:27 -0400

    preliminary template for dashboard pages

--- app/admintest/dashboard/classes/page.tsx
+++ app/admintest/dashboard/classes/page.tsx
@@ -1,107 +1,31 @@
-"use client";
-import { useState } from "react";
-import { classroomColumns, Classroom, Class , classColumns} from "@/app/admintest/components/columns/column-types";
+import { reducedClassColumns, ReducedClass} from "@/app/admintest/components/columns/column-types";
+import { getAllClasses } from '@/app/lib/data-actions'
 import DataTable from "@/app/admintest/components/data-table";
 import { ColumnDef } from "@tanstack/react-table";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
-import HorizontalNav from "@/components/horizontal-nav";
+import { DownloadIcon, PlusIcon, UploadIcon, ColumnsIcon } from "lucide-react";
+import Filter from "@/components/filter";
 
-export default function ClassesPage() {
-    const [selectedTab, setSelectedTab] = useState("classes");
-    const mockClasses: Class[] = [
-        {
-            id: 1,
-            class_name_cn: "Class 1",
-            class_name_en: "Class 1",
-            upgrade_class: "Class 1",
-            class_level: "Class 1",
-            class_type: "Class 1",
-            status: "Active",
-            updateBy: "Class 1",
-            updateAt: new Date(),
-            description: "Notes 1",
-        },
-        {
-            id: 2,
-            class_name_cn: "Class 2",
-            class_name_en: "Class 2",
-            upgrade_class: "Class 2",
-            class_level: "Class 2",
-            class_type: "Class 2",
-            status: "Inactive",
-            updateBy: "Class 2",
-            updateAt: new Date(),
-            description: "Notes 2",
-        },
-        {
-            id: 3,
-            class_name_cn: "中文课",
-            class_name_en: "Chinese Class",
-            upgrade_class: "Class 3",
-            class_level: "Class 3",
-            class_type: "Class 3",
-            status: "Active",
-            updateBy: "Class 3",
-            updateAt: new Date(),
-            description: "Notes 3",
-        },
-        {
-            id: 4,
-            class_name_cn: "英文课",
-            class_name_en: "English Class",
-            upgrade_class: "Class 4",
-            class_level: "Class 4",
-            class_type: "Class 4",
-            status: "Inactive",
-            updateBy: "Class 4",
-            updateAt: new Date(),
-            description: "Notes 4",
-        },
-        {
-            id: 5,
-            class_name_cn: "数学课",
-            class_name_en: "Math Class",
-            upgrade_class: "Class 5",
-            class_level: "Class 5",
-            class_type: "Class 5",
-            status: "Active",
-            updateBy: "Class 5",
-            updateAt: new Date(),
-            description: "Notes 5",
-        },
-    ];
+type searchParams = {
+    page: string;
+    query: string;
+    selectedTab: string;
+}
+export default async function ClassesPage({ 
+    searchParams 
+}: { 
+    searchParams: Promise<searchParams> 
+}) {
+    const { page, query, selectedTab } = await searchParams;
+    
+    const pageSize = 4; // Number of items per page
 
-    const mockClassrooms: Classroom[] = [
-        {
-            room_id: 1,
-            classroom_name: "Classroom 1",
-            seats: 10,
-            status: "Active",
-            notes: "Notes 1",
-        },
-        {
-            room_id: 2,
-            classroom_name: "Classroom 2",
-            seats: 20,
-            status: "Inactive",
-            notes: "Notes 2",
-        },
-        {
-            room_id: 3,
-            classroom_name: "Classroom 3",
-            seats: 30,
-            status: "Active",
-            notes: "Notes 3",
-        },
-        {
-            room_id: 4,
-            classroom_name: "Classroom 4",
-            seats: 40,
-            status: "Inactive",
-            notes: "Notes 4",
-        },
-    ]
+    // Fetch only the data needed for the current page
+    const { classes, totalCount } = await getAllClasses({
+        page: parseInt(page) || 1,
+        pageSize,
+        query
+    });
+
     const tabs = [
         {
             label: "Classes",
@@ -114,9 +38,31 @@ export default function ClassesPage() {
     ]
     return (
         <div className="flex flex-col gap-4">
-            <h1 className="text-2xl font-bold">Classes</h1>
-            <HorizontalNav tabs={tabs} selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-                <DataTable data={mockClasses} columns={classColumns as ColumnDef<Class>[]} />
+            <h1 className="text-3xl font-bold">Class Management</h1>
+            {/* <HorizontalNav tabs={tabs} selectedTab={selectedTab} setSelectedTab={} /> */}
+
+            <div className="flex items-center px-4 py-2 gap-1">
+                <p className="text-lg font-bold">All classes </p>
+                <span className="text-sm text-gray-500">({totalCount})</span>
+            </div>
+            <div className="flex justify-between items-center gap-2 px-4 py-2">
+                <div className="flex gap-2">
+                    <button className="flex items-center gap-2 text-sm text-gray-500 bg-white border border-gray-300 rounded-md px-2 py-1 hover:bg-blue-600 hover:text-white transition-colors duration-200 cursor-pointer"> <ColumnsIcon className="w-4 h-4" /> Select Columns</button>
+                    {/* <button className="flex items-center gap-2 text-sm text-gray-500 bg-white border border-gray-300 rounded-md px-2 py-1 hover:bg-blue-600 hover:text-white transition-colors duration-200 cursor-pointer"> <FilterIcon className="w-4 h-4" /> Filters</button> */}
+                    <Filter columns={reducedClassColumns} />
+                </div>
+                <div className="flex gap-2">        
+                    <button className="flex items-center gap-2 text-sm text-gray-500 bg-white border border-gray-300 rounded-md px-2 py-1 hover:bg-blue-600 hover:text-white transition-colors duration-200 cursor-pointer"> <UploadIcon className="w-4 h-4" /> Import</button>
+                    <button className="flex items-center gap-2 text-sm text-gray-500 bg-white border border-gray-300 rounded-md px-2 py-1 hover:bg-blue-600 hover:text-white transition-colors duration-200 cursor-pointer"> <DownloadIcon className="w-4 h-4" /> Export</button>
+                    <button className="flex items-center gap-2 text-sm text-white bg-blue-600 font-medium px-2 py-1 rounded-md hover:bg-blue-700 transition-colors duration-200 cursor-pointer"> <PlusIcon className="w-4 h-4" /> Add Class</button>
+                </div>
+            </div>
+            <DataTable 
+                data={classes as ReducedClass[]} 
+                columns={reducedClassColumns as ColumnDef<ReducedClass>[]}
+                pageCount={Math.ceil(totalCount / pageSize)}
+                currentPage={parseInt(page) || 1}
+            />
         </div>
     )
 }
