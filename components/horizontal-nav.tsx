c063c01d766cf88c8b1119a376fc615e09054cb7
ryw27 <rywbook06@gmail.com>
2025-05-12 14:15:26 -0400

    fixed horizontal nav, now on client

--- components/horizontal-nav.tsx
+++ components/horizontal-nav.tsx
@@ -1,25 +1,33 @@
+"use client";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 
+interface secionTabs {
+    label: string;
+    href: string;
+}
 type HorizontalNavProps = {
-    tabs: {label: string, value: string}[];
-    selectedTab: string;
-    setSelectedTab: (tab: string) => void;
+    tabs: readonly secionTabs[];
+    activeHref?: string;
 }
 export default function HorizontalNav(
-    {tabs, selectedTab, setSelectedTab}: HorizontalNavProps
+    {tabs, activeHref}: HorizontalNavProps
 ) {
+    const pathname = usePathname();
+    const current = activeHref ?? pathname;
+
+
     return (
         <nav className="flex border-b space-x-6 text-sm font-medium text-gray-500">
             {tabs.map((tab) => (
-                <Link 
-                    key={tab.value} 
-                    href={`/admintest/dashboard/${tab.value}`}
-                    onClick={() => setSelectedTab(tab.value)}
+                <Link
+                    key={tab.href} 
+                    href={tab.href}
                     className={cn(
-                        "border-b-2 border-transparent py-3 px-1 transition-colors",
-                        selectedTab === tab.value && "border-blue-500 text-blue-600"
+                        "border-b-2 border-transparent py-3 px-1 transition-colors cursor-pointer",
+                        current === tab.href && "border-blue-500 text-blue-600"
                     )}
                 >
                     {tab.label}
