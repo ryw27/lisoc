8491381b458722cb590e0019c9cd2e500d976b80
ryw27 <rywbook06@gmail.com>
2025-05-15 00:36:04 -0400

    alert dialog for delete and some sorting logic

--- app/admintest/components/data-table.tsx
+++ app/admintest/components/data-table.tsx
@@ -1,12 +1,13 @@
 "use client";
-import { ColumnDef, useReactTable, getCoreRowModel, flexRender } from "@tanstack/react-table"
-import { useEffect, useState } from "react";
+import { ColumnDef, useReactTable, getCoreRowModel, flexRender, SortingState } from "@tanstack/react-table"
+import { useEffect, useMemo, useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import TableHeader from "@/components/table-header";
 import { FilterableColumn } from "./columns/column-types";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger} from '@/components/ui/alert-dialog'
 
 type DataTableProps<TData> = {
     data: TData[];
@@ -18,6 +19,7 @@ type DataTableProps<TData> = {
 export default function DataTable<TData>({data, columns, totalCount, tableName}: DataTableProps<TData>) {
     const [rowSelection, setRowSelection] = useState({});
     const [columnVisibility, setColumnVisibility] = useState<Record<string, boolean>>({})
+    const [sorting, setSorting] = useState<SortingState>([]);
     
     
     useEffect(() => {
@@ -27,14 +29,21 @@ export default function DataTable<TData>({data, columns, totalCount, tableName}:
                 // Use stored visibility if available
                 setColumnVisibility(JSON.parse(storedVisibility));
             } else {
-                console.log("hello");
-                const initialVisibility: Record<string, boolean> = {}
+                const initialVisibility: Record<string, boolean> = {};
+                
+                // First, set all columns to false (hidden)
                 columns.forEach((column) => {
-                    if (column.enableHiding !== false || column.id === 'select' || column.id == 'edit') {
+                    initialVisibility[column.id] = false;
+                });
+                
+                // Then, set special columns to true (visible)
+                columns.forEach((column) => {
+                    if (column.enableHiding === false || column.id === 'select' || column.id === 'edit') {
                         initialVisibility[column.id] = true;
                     }
-                })
-                console.log(initialVisibility);
+                });
+                
+                setColumnVisibility(initialVisibility);
             }
         } catch (error) {
             console.error("Error parsing column visibility");
@@ -45,14 +54,14 @@ export default function DataTable<TData>({data, columns, totalCount, tableName}:
         if (Object.keys(columnVisibility).length > 0) {
             localStorage.setItem(`${tableName} Columns`, JSON.stringify(columnVisibility));
         }
-    }, [columnVisibility, tableName]);
+    }, [columnVisibility, tableName, columns]);
 
     const router = useRouter();
     const pathname = usePathname();
     const searchParams = useSearchParams();
 
     // Create our checkbox column
-    const selectionColumn: ColumnDef<TData>[] = [
+    const selectionColumn: ColumnDef<TData>[] = useMemo(() => [
         {
             id: "select",
             header: ({ table }) => {
@@ -105,10 +114,10 @@ export default function DataTable<TData>({data, columns, totalCount, tableName}:
                 )
             }
         }
-    ];
+    ], []);
 
     // Create edit column
-    // const editColumn: ColumnDef<TData>[] = [
+    // const editColumn: ColumnDef<TData>[] = useMemo(() => [
     //     {
     //         id: "edit",
     //         header: "Edit",
@@ -116,25 +125,51 @@ export default function DataTable<TData>({data, columns, totalCount, tableName}:
     //             return <div>Edit</div>
     //         }
     //     }
-    // ]
+    // ], [])
     // Combine selection column with provided columns
-    const allColumns = [...selectionColumn, ...columns];
+    const allColumns = useMemo(() => [...selectionColumn, ...columns], [columns])
+
+    // Set sorting 
+    useEffect(() => {
+        const sortBy = searchParams.get('sortBy');
+        const sortOrder = searchParams.get('sortOrder');    
+        if (sortBy && sortOrder) {
+            setSorting([{id: sortBy, desc: sortOrder === 'desc'}]);
+        } else {
+            setSorting([]);
+        }
+    }, [searchParams.toString()]);
+
+    useEffect(() => {
+        console.log("sorting: ", sorting);
+        // console.log("sortBy: ", sorting[0].id)
+        // console.log("sortBy: ", sorting[0].desc)
+        if (sorting.length > 0) {
+            if (sorting[0].id && sorting[0].desc) {
+                const params = new URLSearchParams(searchParams.toString());
+                params.set('sortBy', sorting[0].id);
+                params.set('sortOrder', sorting[0].desc ? 'desc' : 'asc');
+                console.log(params);
+                router.replace(`?${params.toString()}`);
+            }
+        }
+    }, [sorting]) 
 
     // Pagination variables
     const currentPage = Number(searchParams.get('page')) || 1;
-    const pageSize = Number(searchParams.get('pagesize')) || 10;
+    const pageSize = Number(searchParams.get('pageSize')) || 10;
     const pageCount = Math.ceil(totalCount / pageSize);
 
     // Function to create URL with updated page parameter
     const createPageURL = (pageNumber: number) => {
-        const params = new URLSearchParams(searchParams);
+        const params = new URLSearchParams(searchParams.toString());
         params.set('page', pageNumber.toString());
         return `${pathname}?${params.toString()}`;
     };
 
     // Function to handle page change
     const handlePageChange = (pageNumber: number) => {
-        router.push(createPageURL(pageNumber));
+        router.replace(createPageURL(pageNumber));
     };
 
     // Editing functions
@@ -149,6 +184,7 @@ export default function DataTable<TData>({data, columns, totalCount, tableName}:
                 pageIndex: 0, // Always show first page in the UI since actual pagination is server-side
                 pageSize: data.length // Show all data we received for this page
             },
+            sorting,
             columnVisibility // Select which columns to show
         },
         enableRowSelection: true,
@@ -165,7 +201,9 @@ export default function DataTable<TData>({data, columns, totalCount, tableName}:
         console.log("mass delete");
     }
 
-    
+    // Memoize the columns passed to TableHeader
+    const memoizedTableColumns = useMemo(() => table.getAllColumns(), [table]);
+
     return (
         <div className="bg-white border-b border-gray-200">
             {/* Alert for row selection*/ }
@@ -173,17 +211,36 @@ export default function DataTable<TData>({data, columns, totalCount, tableName}:
                 {Object.keys(rowSelection).length > 0 && (
                     <div className="flex justify-between items-center bg-blue-50 border border-blue-200 text-blue-700 px-4 py-2 rounded-md text-sm">
                         <p>{Object.keys(rowSelection).length} {Object.keys(rowSelection).length === 1 ? 'row' : 'rows'} selected</p>
-                        <button 
-                            className="text-red-500 font-medium cursor-pointer"
-                            onClick={handleMassDelete}
-                        >
-                            Delete
-                        </button>
+                        <AlertDialog>
+                            <AlertDialogTrigger asChild>
+                                <button 
+                                    className="text-red-500 font-medium cursor-pointer"
+                                >
+                                    Delete
+                                </button>
+                            </AlertDialogTrigger>
+                            <AlertDialogContent>
+                                <AlertDialogHeader>
+                                    <AlertDialogTitle>
+                                        Are you  absolutely sure?
+                                    </AlertDialogTitle>
+                                    <AlertDialogDescription>
+                                        This action cannot be undone. This will permanently delete the selected rows from 
+                                        the database.
+                                    </AlertDialogDescription>
+                                </AlertDialogHeader>
+                                <AlertDialogFooter>
+                                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                    <AlertDialogAction onClick={handleMassDelete}>Continue</AlertDialogAction>
+                                </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialog>
+
                     </div>
                 )}
             </div>
             <TableHeader 
-                columns={table.getAllColumns()}
+                columns={memoizedTableColumns}
                 columnDefs={columns}
             />
 
@@ -197,11 +254,22 @@ export default function DataTable<TData>({data, columns, totalCount, tableName}:
                                 {headerGroup.headers.map((header) => (
                                     <th 
                                         key={header.id}
-                                        className={`px-3 py-3 text-left text-xs font-bold text-black uppercase tracking-wider ${
+                                        className={`whitespace-nowrap cursor-pointer px-3 py-3 text-left text-xs font-bold text-black uppercase tracking-wider ${
                                             header.id === 'select' ? 'w-12' : ''
                                         }`}
+                                        onClick={() => {
+                                            if ((header.id !== 'select') && header.column.getCanSort()) {
+                                                const direction = sorting[0]?.id === header.id && sorting[0]?.desc === false ? true : false; // true it's desc, false it's asc
+                                                console.log("hi", header.id);
+                                                setSorting([{id: header.id, desc: direction}]);
+                                            }
+                                        }}
+                                        aria-sort={sorting.length > 0 ? sorting[0].id === header.id ? sorting[0].desc === true ? 'descending' : 'ascending' : 'none' : 'none'}
                                     >
                                         {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                                        {header.id === sorting[0]?.id && (
+                                            <span>{sorting[0].desc === false ? ' ↑' : ' ↓'}</span>
+                                        )}
                                     </th>
                                 ))}
                             </tr>
