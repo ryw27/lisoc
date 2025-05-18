d6686e7e830818d98904356d15ead026fe235e51
ryw27 <rywbook06@gmail.com>
2025-05-18 13:48:36 -0400

    better and working add, update, and delete for class. Highly transferable i think

--- app/admintest/dashboard/(class-pages)/class-helpers.ts
+++ app/admintest/dashboard/(class-pages)/class-helpers.ts
@@ -1,28 +1,39 @@
 import { db } from "@/app/lib/db/index";
 import { classes, classregistration, classrooms, classtype } from "@/app/lib/db/schema";
-import { generateColumnDefs } from "@/app/lib/data-actions";
-import { createInsertSchema} from "drizzle-zod";
-import { z, ZodError, ZodIssue } from 'zod';
-import { eq, inArray, getTableColumns, InferSelectModel, max, and } from 'drizzle-orm';
+import { deleteRows, generateColumnDefs } from "@/app/lib/data-actions";
+import { createInsertSchema, createUpdateSchema } from "drizzle-zod";
+import { z, ZodError } from 'zod';
+import { eq, and } from 'drizzle-orm';
 import { redirect } from 'next/navigation';
-import { AnyPgTable } from "drizzle-orm/pg-core";
 import { formatISO } from "date-fns";
 import { randomUUID } from "crypto";
-import { requireRole } from '@/app/lib/actions'
+import { revalidatePath } from "next/cache";
+// import { requireRole } from '@/app/lib/actions'
 
-// export function generateFormSchema()
+
+
+
+//----------------------------------------------------------------------------------------
+// CLASSES
+//----------------------------------------------------------------------------------------
+
+
+// Form Schema: Form schema is not necessarily the same as database schema
 export const classFormSchema = z.object({
     classnamecn: z.string().min(1),
     classnameen: z.string().min(1),
     typeid: z.coerce.number().int().positive(), // Select strings from dropdowon, but values are numbers increasing from 1 correctly
     classno: z.coerce.number().int().nonnegative(), // Coerce string from form to number
-    sizelimits: z.coerce.number().int().positive().optional(), // Coerce string from form to number, optional
+    sizelimits: z.coerce.number().int().nonnegative().optional(), // Coerce string from form to number, optional
     status: z.enum(["Active", "Inactive"]), 
     classupid: z.string().min(1), // Start as a string, selected class name. id found later
     description: z.string().optional() // optional
 })
 
-export type newClass = typeof classes.$inferSelect
+// Type of any class insertions: for compile-time type checking
+//inferSelect is used to get the type of the table, classes is just the drizzle schema table
+export type Class = typeof classes.$inferSelect
+// Add a class from form data and insert into database
 export async function addClass(formData: FormData) {
     'use server';
     // make sure user is logged in and has correct roles
@@ -60,23 +71,26 @@ export async function addClass(formData: FormData) {
                 
             if (!upgrade || upgrade.length === 0) throw new Error("Invalid upgrade class");
 
-            const newclassid = Number(randomUUID().slice(0, 8)); 
+
+            // TODO: Change to increase maximum. It's just nicer.
+            const newclassid = parseInt(randomUUID().slice(0, 4), 16); // Convert hex string to integer, using only 4 chars to ensure it fits
             const newclass = {
                 ...form,
                 classid: newclassid,
                 classupid: upgrade[0].classid, // Use the found class ID
+                classno: String(form.classno), // Numeric requires string for some reason
                 lastmodify: formatISO(new Date()),
                 createon: formatISO(new Date()),
                 updateon: formatISO(new Date()),
                 createby: user,
                 updateby: user
-            };
+            } as Class;
 
             const parsed = classInsertSchema.parse(newclass);
             await db.insert(classes).values(parsed);
         });  
-
-        redirect(`/admintest/dashboard/classes`);
+        
+        redirect('/admintest/dashboard/class-view');
     } catch (error) {
         console.error(error);
         if (error instanceof ZodError) {
@@ -90,7 +104,11 @@ export async function addClass(formData: FormData) {
 }
 
 
-export async function updateClass(formData: FormData) {
+// Update an existing class in the database. 
+// Uses same form schema as insertion. Existing values will fill in for non-updated values as default values
+
+// TODO: This is probably not super efficient right now.
+export async function updateClass(formData: FormData, input_id:string) {
     'use server';
     // make sure user is logged in and has correct roles
     // const session = await requireRole(["admin"])
@@ -101,22 +119,10 @@ export async function updateClass(formData: FormData) {
     const user = "testuser";
 
     //drizzle-zod to validate that this is a valid object that can be inserted into database
-    const classInsertSchema = createInsertSchema(classes);
+    const classUpdateSchema = createUpdateSchema(classes);
     try {
         const form = classFormSchema.parse(Object.fromEntries(formData));
         await db.transaction(async (tx) => {
-            // Check whether the class already exists
-            const exists = await tx
-                    .select()
-                    .from(classes)
-                    .where(and(
-                        eq(classes.classnamecn, form.classnamecn),
-                        eq(classes.status, "Active")
-                    ))
-                    .limit(1);
-            if (exists.length) {
-                throw new Error("Class already exists");
-            }
 
             // Find the class that matches the selected class name (not id)
             const upgrade = await tx
@@ -127,53 +133,48 @@ export async function updateClass(formData: FormData) {
                 
             if (!upgrade || upgrade.length === 0) throw new Error("Invalid upgrade class");
 
-            const newclassid = Number(randomUUID().slice(0, 8)); 
             const newclass = {
                 ...form,
-                classid: newclassid,
                 classupid: upgrade[0].classid, // Use the found class ID
+                classindex: null,
+                ageid: null,
+                classno: String(form.classno),
                 lastmodify: formatISO(new Date()),
-                createon: formatISO(new Date()),
+                // createon: formatISO(new Date()), // no change for this on update
                 updateon: formatISO(new Date()),
-                createby: user,
+                // createby: user, // no change for this on update
                 updateby: user
-            };
+            } as Class;
 
-            const parsed = classInsertSchema.parse(newclass);
-            await db.insert(classes).values(parsed);
+            const parsed = classUpdateSchema.parse(newclass);
+            await db.update(classes).set(parsed).where(eq(classes.classid, parseInt(input_id)));
         });  
 
+        // TODO: these redirects don't work for some reason
         redirect(`/admintest/dashboard/classes`);
     } catch (error) {
         console.error(error);
+        // TODO: Better error handling
         if (error instanceof ZodError) {
-            redirect(`/admintest/dashboard/add-class/?error=${encodeURIComponent(error.errors[0].message)}`);
+            redirect(`/admintest/dashboard/class-view/${input_id}/edit-class/?error=${encodeURIComponent(error.errors[0].message)}`);
         } else if (error instanceof Error) {
-            redirect(`/admintest/dashboard/add-class/?error=${encodeURIComponent(error.message)}`);
+            redirect(`/admintest/dashboard/class-view/${input_id}/edit-class/?error=${encodeURIComponent(error.message)}`);
         } else {
-            redirect(`/admintest/dashboard/add-class/?error=${encodeURIComponent('An unknown error occurred')}`);
+            redirect(`/admintest/dashboard/class-view/${input_id}/edit-class/?error=${encodeURIComponent('An unknown error occured')}`);
         }
     } 
 }
 
 
-export async function deleteRows<T extends AnyPgTable, PrimaryKey extends keyof T['_']['columns'] & string>
-(
-    table: T, 
-    pk: PrimaryKey,
-    values: InferSelectModel<T>[PrimaryKey] | InferSelectModel<T>[PrimaryKey][] //number, number[]
-) {
-    const valueslist = Array.isArray(values) ? values : [values]
-    const column = getTableColumns(table)[pk]
-    return await db.delete(table).where(inArray(column, valueslist)).returning();
+// Delete classes. Use delete rows helper function
+export async function deleteClasses(ids: number[]) {
+    'use server';
+    await deleteRows(classes, 'classid', ids);
+    revalidatePath('/admintest/dashboard/class-view');
 }
 
-//----------------------------------------------------------------------------------------
-// CLASSES
-//----------------------------------------------------------------------------------------
 
-//inferSelect is used to get the type of the table, classes is just the drizzle schema table
-export type Class = typeof classes.$inferSelect;
+
 export const classColumns = generateColumnDefs<Class>(classes, {
     classid: {
         header: "Class ID",
@@ -227,11 +228,13 @@ export const classColumns = generateColumnDefs<Class>(classes, {
     },
 })
 
+// Get all classes
 export async function getAllClasses() {
     const result = await db.select().from(classes);
     return result;
 }
 
+// Get classes with filter and pagination restrictions
 export async function getClasses(page: number, pageSize: number, query:string = "") {
     const result = await db.select().from(classes);
     const size = result.length;
