a25165b15c7459ee85483ab286eea3b709b34091
ryw27 <rywbook06@gmail.com>
2025-05-18 13:46:25 -0400

    changed link names

---
+++ app/admintest/dashboard/(class-pages)/class-view/page.tsx
@@ -0,0 +1,93 @@
+import { getClasses, classColumns, Class } from '@/app/admintest/dashboard/(class-pages)/class-helpers'
+import DataTable from "@/app/admintest/components/data-table";
+import { PlusIcon } from "lucide-react";
+import Link from 'next/link';
+import HorizontalNav from '@/components/horizontal-nav';
+import { FilterableColumn } from '@/app/admintest/components/columns/column-types';
+import { deleteClasses } from '@/app/admintest/dashboard/(class-pages)/class-helpers';
+import { Row } from '@tanstack/react-table';
+
+//--------------------------------
+//-- Types and Helpers 
+//--------------------------------
+
+type SearchParams = {
+    page?: string;
+    pageSizes?: string;
+    query?: string;
+    [key: string]: string | undefined; // Allow for additional query parameters
+}
+
+function handleFilterQuery(query: string) {
+    return Object.entries(query)
+        .filter(([key]) => key !== 'page' && key !== 'pageSizes')
+        .map(([key, val]) => `${key}=${val}`)
+        .join('&') || '';
+}
+
+//--------------------------------
+//-- Page Component for classes
+//--------------------------------
+
+export default async function ClassesPage({ 
+    searchParams 
+}: { 
+    searchParams: Promise<SearchParams> 
+}) {
+    // Await searchParams before using it
+    const params = await searchParams;
+    
+    // Parse and validate search parameters with defaults
+    const page = parseInt(params.page || '1');
+    const pageSize = parseInt(params.pageSizes || '10');
+    
+    // Validate parsed values
+    const validPage = isNaN(page) || page < 1 ? 1 : page;
+    const validPageSize = isNaN(pageSize) || pageSize < 1 ? 10 : pageSize;
+    // console.log("valid", validPage, validPageSize);
+
+    // Build query
+    const query = handleFilterQuery(params.query || '');
+
+    // Fetch only the data needed for the current page
+    const { classes, totalCount } = await getClasses(
+        validPage,
+        validPageSize,
+        query
+    );
+
+
+    return (
+        <div className="flex flex-col gap-4">
+            <div className="flex justify-between items-center">
+                <h1 className="text-3xl font-bold">View Classes</h1>
+                <Link 
+                    href="/admintest/dashboard/add-class" 
+                    className="flex items-center gap-2 mr-4 text-sm text-white bg-blue-600 font-medium px-3 py-2 rounded-md hover:bg-blue-700 transition-colors duration-200 cursor-pointer"
+                >
+                    <PlusIcon className="w-4 h-4" /> Add Class
+                </Link>
+            </div>
+            <HorizontalNav
+                tabs={[
+                { label: 'Classes', href: '/admintest/dashboard/classes' },
+                { label: 'Classrooms', href: '/admintest/dashboard/classrooms' },
+                ]}
+                activeHref={`/admintest/dashboard/classes`}
+            />
+            <div className="flex items-center px-4 py-2 gap-1">
+                <p className="text-lg font-bold">All classes </p>
+                <span className="text-sm text-gray-500">({totalCount})</span>
+            </div>
+            
+            <DataTable 
+                data={classes as Class[]} 
+                columns={classColumns as FilterableColumn<Class>[]}
+                totalCount={totalCount}
+                tableName="Classes"
+                primaryKey="classid"
+                deleteAction={deleteClasses}
+            />
+        </div>
+    )
+}
