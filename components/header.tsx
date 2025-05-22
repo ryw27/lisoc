441d403b3f6bf865b843a2d7c739e89b140c2b50
ryw27 <rywbook06@gmail.com>
2025-05-22 01:03:09 -0400

    small styling changes to header

--- components/header.tsx
+++ components/header.tsx
@@ -15,17 +15,12 @@ export default function Header() {
 
     return (
         <header className="bg-white border-gray-200 sticky top-0 z-10">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-16">
-                    {/* Left side with logo */}
-                    {/* <div className="flex items-center">
-                        {/* <Link href="/" className="flex-shrink-0">
-                            <Image src="/lisoc.png" alt="LISOC Logo" width={40} height={40} className="h-10 w-auto" />
-                        </Link>
-                    </div>
+            <div className="px-4">
+                <div className="flex items-center h-16 w-full">
+
 
                     {/* Center with search bar */}
-                    <div className="flex-1 max-w-xl px-6">
+                    <div className="flex-1 max-w-2xl px-4 mx-auto">
                         <div className="relative">
                             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                 <Search className="h-4 w-4 text-gray-400" />
@@ -39,7 +34,7 @@ export default function Header() {
                     </div>
 
                     {/* Right side with actions */}
-                    <div className="flex items-center space-x-4">
+                    <div className="flex items-center space-x-4 ml-auto">
                         {/* Language toggle */}
                         <button 
                             onClick={toggleLanguage}
