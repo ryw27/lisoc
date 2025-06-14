7e96a0bd2806c53480acc642b3ba7dc845a6900c
ryw27 <rywbook06@gmail.com>
2025-06-13 21:05:17 -0400

    ok should be fixed now, but a lot isn't done yet

--- app/admintest/dashboard/teachers/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function TeachersPage() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- app/admintest/dashboard/transactions/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function TransactionsPage() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
