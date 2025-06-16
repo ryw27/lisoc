3534b8f5c8a673ada8e54a886a0256ae7a71c14b
ryw27 <rywbook06@gmail.com>
2025-06-15 21:59:08 -0400

    fixing types from type changes and changing to main path instead of revalidate path

--- app/lib/entity-config.ts
+++ app/lib/entity-config.ts
@@ -1,32 +1,30 @@
-import { ColKey, ColVal, PKName, PKVal, Table, TableName } from "./entity-types";
-import { z, ZodSchema } from "zod";
+import { PKName, PKVal, Table, TableName } from "./entity-types";
+import { ZodSchema } from "zod";
 import { FilterableColumn } from "./column-actions";
 import { makeOperations, type Extras, type enrichFields, type uniqueCheckFields} from "./data-actions"
 import { parsedParams } from "./handle-params";
-import { PgTransaction } from "drizzle-orm/pg-core";
-import { InferInsertModel, InferSelectModel } from "drizzle-orm";
-import * as schema from "./db/schema";
+import { InferSelectModel } from "drizzle-orm";
 
-// The only unique things you need for each entity are form schema, header names, revalidate path, and enrich/unique/extras 
+// The only unique things you need for each entity are form schema, header names, main path, and enrich/unique/extras 
 // EntityConfig is a generic type that takes a table name and returns an object with the following properties:
 // tableName: The name of the table
 // primaryKey: The name of the primary key column
 // formSchema: The Zod schema for the form
-// revalidatePath: The path to revalidate
+// mainPath: The path to revalidate
 // ops: An object encompassing all CRUD operations for the table
 export interface EntityConfig<N extends TableName, T extends Table<N>> {
     table: T; // The table object, i.e. classes, adminrole, etc.
     tableName: N; // The name of the table, i.e. classes, adminrole, etc.
-    primaryKey: PKName<N>; // The name of the primary key column, i.e. classid, roleid, etc.
+    primaryKey: PKName<N, T>; // The name of the primary key column, i.e. classid, roleid, etc.
     formSchema: ZodSchema; // The Zod schema for the form, used for editing, adding, and updating
-    revalidatePath: string; // The path to revalidate, used for revalidating the page after a mutation
+    mainPath: string; // The path to revalidate, used for revalidating the page after a mutation
     columns: FilterableColumn<InferSelectModel<T>>[]; // The column definitions for the table, used for the table headers and filtering
     ops: {
         allRows: () => Promise<InferSelectModel<T>[]>; // Get all rows from the table
         idRow: (id: PKVal<N>) => Promise<InferSelectModel<T>>; // Get a row from the table by its primary key
         pageRows: (opts: parsedParams) => Promise<{ rows: InferSelectModel<T>[]; totalCount: number }>; // Get rows from the table with pagination
-        insertRow: (formData: FormData, insertExtras: Extras<N, T>) => Promise<InferInsertModel<T>>; // Insert a new row into the table
-        updateRow: (id: PKVal<N>, formData: FormData, updateExtras: Extras<N, T>) => Promise<InferInsertModel<T>>; // Update a row in the table
+        insertRow: (formData: FormData) => Promise<void>; // Insert a new row into the table
+        updateRow: (id: PKVal<N>, formData: FormData) => Promise<void>; // Update a row in the table
         deleteRows: (id: PKVal<N>[]) => Promise<InferSelectModel<T>[]>; // Delete a row from the table
     }
 }
@@ -35,11 +33,11 @@ export interface EntityConfig<N extends TableName, T extends Table<N>> {
 interface makeEntityProps<N extends TableName, T extends Table<N>, FormSchema extends ZodSchema> {
     table: T;
     tableName: N;
-    primaryKey: PKName<N>;
+    primaryKey: PKName<N, T>;
     formSchema: FormSchema;
-    revalidatePath: string;
+    mainPath: string;
 	enrichFields: enrichFields<FormSchema>[], 
-	uniqueConstraints?: uniqueCheckFields<N, T>[],
+	uniqueConstraints?: uniqueCheckFields<N, T, FormSchema>[],
     insertExtras?: Extras<N, T>,
     updateExtras?: Extras<N, T>,
     columns: FilterableColumn<InferSelectModel<T>>[];
@@ -51,7 +49,7 @@ export function makeEntity<N extends TableName, T extends Table<N>, FormSchema e
         tableName,
         primaryKey,
         formSchema,
-        revalidatePath,
+        mainPath,
         enrichFields,
         uniqueConstraints,
         insertExtras,
@@ -60,14 +58,14 @@ export function makeEntity<N extends TableName, T extends Table<N>, FormSchema e
     }: makeEntityProps<N, T, FormSchema>
 ): EntityConfig<N, T> {
     const { allRows, idRow, pageRows, insertRow, updateRow, deleteRows } = 
-        makeOperations(tableName, table, formSchema, revalidatePath, enrichFields, uniqueConstraints, insertExtras, updateExtras);
+        makeOperations(tableName, table, primaryKey, formSchema, mainPath, enrichFields, uniqueConstraints, insertExtras, updateExtras);
 
     const config: EntityConfig<N, T> = {
         table,
         tableName,
         primaryKey,
         formSchema,
-        revalidatePath,
+        mainPath,
         columns,
         ops: {
             allRows,
