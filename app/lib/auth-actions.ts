97edbc0f382ca01bc79ae616a27af7b841fefce8
ryw27 <rywbook06@gmail.com>
2025-06-16 23:04:12 -0400

    better require role and handling separate admin login

--- app/lib/auth-actions.ts
+++ app/lib/auth-actions.ts
@@ -4,33 +4,45 @@ import { auth, signIn, signOut } from "./auth";
 import { redirect } from "next/navigation";
 
 
-//Login function
+// Login function
 export async function login(formData: FormData) {
-    const result = await signIn("credentials", {
+    const isAdminForm = !!formData.get("username");
+
+    const provider = isAdminForm ? "admin-credentials" : "user-credentials";
+    const result = await signIn(provider, {
+        username: formData.get("username")?.toString().trim(),
         email: formData.get("email")?.toString().trim(),
         password: formData.get("password")?.toString(),
         redirect: false,
     });
 
     if (!result.ok) {
-        redirect("/login?error=Invalid credentials");
+        isAdminForm ? redirect("/login/admin?error=Invalid credentials") : redirect("/login?error=Invalid credentials");
     }
-    redirect("/dashboard")
+
+    isAdminForm ? redirect("/admin/dashboard") : redirect("/dashboard");
 }
 
-//Logout function   
+// Logout function   
 export async function logout() {
     await signOut();
     redirect("/");
 }
 
 
-//Helper function to require a specific role to access a page or API route
-export async function requireRole(allowed: ("ADMIN" | "TEACHER" | "PARENT")[]) {
+// Helper function to require a specific role to access a page or API route
+export async function requireRole(allowed: ("ADMIN" | "TEACHER" | "FAMILY")[]) {
     const session = await auth();
-    if (!session || !allowed.includes(session.user.role as "ADMIN" | "TEACHER" | "PARENT")) {
-        redirect("/login");
+    if (!session && allowed.includes("ADMIN")) {
+        redirect("/login/admin") // Not logged in and trying to access admin pages
+    } else if (!session) {
+        redirect("login") // Not logged in and trying to access any other pages
+    } else if (allowed.includes("ADMIN") && session.user.role != "ADMIN") {
+        redirect("/unauthorized") // Logged in as user and trying to access admin pages
+    } else if (!allowed.includes(session.user.role as "ADMIN" | "TEACHER" | "FAMILY")) {
+        redirect("/unauthorized") // Logged in and trying to access pages you can't
     }
+
     return session;
 }
 
