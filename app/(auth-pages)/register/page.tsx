8cc6b30e51af9ec494015811be5bac7f46638e18
ryw27 <rywbook06@gmail.com>
2025-06-18 22:30:25 -0400

    register page driver

---
+++ app/(auth-pages)/register/page.tsx
@@ -0,0 +1,25 @@
+import { emailToCode, checkCode, register } from '@/app/lib/auth-lib/auth-actions';
+import RegisterForm from '@/components/register-form';
+import FamilyForm from '@/components/family-form';
+
+
+// const emailSchema = z.object({
+//     email: z
+//         .string()
+//         .min(1, { message: "This field has to be filled"})
+//         .email("This is not a valid email")
+//         .refine(async (e) => {
+//             return await checkExistence(e);
+//         }, "This email already exists. Please login")
+// })
+
+export default function RegisterPage() {
+    return (
+        <RegisterForm 
+            register={register}
+            requestCode={emailToCode}
+            checkCode={checkCode}
+            familyForm={<FamilyForm />}
+        />
+    )
+}
