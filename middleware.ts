6b8486deac73d908bef2ccdb36974c06ae0eb8bf
ryw27 <rywbook06@gmail.com>
2025-06-23 22:54:32 -0400

    Middleware change with JWTs

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,32 @@
+import NextAuth, { NextAuthRequest } from 'next-auth';
+import authConfig from './auth.config';
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
-// Simple middleware for initial auth check
-export function middleware(request: NextRequest) {
-    // Log to confirm middleware is running
-    
-    const pathname = request.nextUrl.pathname;
-    
-    // Skip auth check for API routes
-    if (pathname.startsWith('/api')) {
-        return NextResponse.next();
-    }
-    
+
+const { auth } = NextAuth(authConfig);
+export default auth((req:NextAuthRequest) => {
+    const pathname = req.nextUrl.pathname;
     // Only check auth for protected routes
-    if (pathname.startsWith('/dashboard') || pathname.startsWith('/admin')) {
-        // Check for the session cookie (this doesn't validate the cookie, just checks existence)
-        const sessionCookie = request.cookies.get('__Host.authjs.session');
-        
-        if (!sessionCookie) {
-            if (pathname.startsWith('/admin')) {
-				return NextResponse.redirect(new URL('/login/admin', request.url)) 
-			} else {
-				return NextResponse.redirect(new URL('/login', request.url));
-			} 
+    if (!req.auth || new Date(req.auth.expires) < new Date(Date.now())) {
+        if (pathname.startsWith('/admin')) {
+            return NextResponse.redirect(new URL('/login/admin', req.url)) 
+        } else if (pathname.startsWith('/teacher')) {
+            return NextResponse.redirect(new URL('/login/teacher', req.url));
+        } else {
+            return NextResponse.redirect(new URL('/login', req.url));
         }
     }
-    
+
+    if (pathname.startsWith('/admin') && req.auth.user.role != "ADMIN") {
+        return NextResponse.redirect(new URL('/forbidden', req.url));
+    } else if (pathname.startsWith('/teacher') && req.auth.user.role != "TEACHER") {
+        return NextResponse.redirect(new URL('/forbidden', req.url));
+    } else if (pathname.startsWith('/dashboard') && req.auth.user.role != "FAMILY") {
+        return NextResponse.redirect(new URL('/forbidden', req.url));
+    }
+
     return NextResponse.next();
-}
+})
 
 // Only match dashboard and admin routes
 export const config = {
