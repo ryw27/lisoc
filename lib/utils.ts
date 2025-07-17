fccf2d17bb44096ee50fc96f16050407256ca1e8
ryw27 <rywbook06@gmail.com>
2025-07-16 23:25:55 -0400

    EST timezone helper

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { TZDate } from "@date-fns/tz";
+import { formatInTimeZone } from 'date-fns-tz';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  	return twMerge(clsx(inputs))
+}
+
+// ----------------------------------------------------------------
+// TIMEZONE UTILITIES
+// ----------------------------------------------------------------
+
+
+export function toESTString(date: Date) {
+	return formatInTimeZone(date, 'America/New_York', "yyyy-MM-dd'T'HH:mm:ssXXX")
 }
