cc76d3712500934a1998fa853e10efa128df42d9
ryw27 <rywbook06@gmail.com>
2025-07-25 16:58:42 -0400

    Rollback distributed registrations back to reg staging class

--- app/lib/semester/sem-actions.ts
+++ app/lib/semester/sem-actions.ts
@@ -12,6 +12,7 @@ import { toESTString } from "@/lib/utils";
 import { studentSchema } from "./sem-schemas";
 import { familyObject } from "@/app/admintest/data/(people-pages)/families/family-helpers"; 
 import { ArrowDownRightFromCircle } from "lucide-react";
+import ClassroomPage from "@/app/admintest/data/(class-pages)/classrooms/[roomid]/page";
 
 const SEMESTERONLY_SUITBALETERM_FOREIGNKEY = 2;
 const REGISTRATION_FEE = 0;
@@ -695,6 +696,37 @@ export async function distributeStudents(data: fullRegClass, moved: { studentid:
                     eq(classregistration.seasonid, data.arrinfo.seasonid),
                     eq(classregistration.arrangeid, data.arrinfo.arrangeid as number) // Should be in registrations. If not, this is the wrong function
                 ))
+        }
+    })
+}
+
+export async function rollbackDistribution(data: fullRegClass) {
+    await db.transaction(async (tx) => {
+        if (!data.arrinfo.arrangeid || data.arrinfo.arrangeid === 0) {
+            throw new Error("Arrange identifier does not exist in data in rollback.")
+        }
+
+        for (const classroom of data.classrooms) {
+            // console.log(student, data.arrinfo.arrangeid, data.arrinfo.classid)
+            if (!classroom.arrinfo.arrangeid || classroom.arrinfo.arrangeid === 0) {
+                throw new Error("Arrange identifier does not exist in classroom in rollback");
+            }
+            for (const student of classroom.students) {
+                const [test] = await tx
+                    .update(classregistration)
+                    .set({
+                        arrangeid: data.arrinfo.arrangeid,
+                        classid: data.arrinfo.classid,
+                        lastmodify: toESTString(new Date())
+                    })
+                    .where(and(
+                        eq(classregistration.studentid, student.studentid), 
+                        eq(classregistration.seasonid, data.arrinfo.seasonid),
+                        eq(classregistration.arrangeid, classroom.arrinfo.arrangeid) 
+                    ))
+                    .returning()
+                console.log("test: ", test);
+            }
         }
     })
 }
