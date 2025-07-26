6c18a17a13d68bd3e7e74d091b3950108ce54b60
ryw27 <rywbook06@gmail.com>
2025-07-25 23:29:07 -0400

    Allow admin to transfer students

--- app/lib/semester/sem-actions.ts
+++ app/lib/semester/sem-actions.ts
@@ -731,6 +731,50 @@ export async function rollbackDistribution(data: fullRegClass) {
     })
 }
 
+export async function adminTransferStudent(regid: number, orgArrange: uiClasses, newArrange: uiClasses) {
+    await db.transaction(async (tx) => {
+        const [newReg] = await tx
+            .update(classregistration)
+            .set({
+                arrangeid: newArrange.arrangeid as number,
+                classid: newArrange.classid
+            })
+            .where(eq(classregistration.regid, regid))
+            .returning()
+
+        // Update family balance
+        // TODO: Factor in whether it's a half or whole semester (Whole classes can be half registered )
+        let orgPrice = 0;
+        if (orgArrange.suitableterm === 2) {
+            orgPrice += Number(orgArrange.tuitionH) + Number(orgArrange.specialfeeH) + Number(orgArrange.bookfeeH) - (orgArrange.waiveregfee ? 0 : REGISTRATION_FEE);
+        } else {
+            orgPrice += Number(orgArrange.tuitionW) + Number(orgArrange.specialfeeW) + Number(orgArrange.bookfeeW) - (orgArrange.waiveregfee ? 0 : REGISTRATION_FEE);
+        }
+        let newPrice = 0;
+        if (newArrange.suitableterm === 2) {
+            newPrice += Number(newArrange.tuitionH) + Number(newArrange.specialfeeH) + Number(newArrange.bookfeeH) - (newArrange.waiveregfee ? 0 : REGISTRATION_FEE);
+        } else {
+            newPrice += Number(newArrange.tuitionW) + Number(newArrange.specialfeeW) + Number(newArrange.bookfeeW) - (newArrange.waiveregfee ? 0 : REGISTRATION_FEE);
+        }
+
+        const updated = await tx.query.familybalance.findFirst({
+            where: (fb, { and, eq }) => and(eq(fb.familyid, newReg.familyid), eq(fb.seasonid, newReg.seasonid))
+        });
+
+        if (!updated) {
+            throw new Error("Family balance not found");
+        }
+
+        await tx
+            .update(familybalance)
+            .set({
+                tuition: (Number(updated.tuition) - orgPrice + newPrice).toString(),
+                totalamount: (Number(updated.totalamount) - orgPrice + newPrice).toString()
+            })
+            .where(and(eq(familybalance.familyid, newReg.familyid), eq(familybalance.seasonid, newReg.seasonid)));
+    })
+}
+
 export async function dropRegistration(regid: number, orgTuition: number) {
     await db.transaction(async (tx) => {
         const reg = await tx.query.classregistration.findFirst({
