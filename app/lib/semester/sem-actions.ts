9fe4d21ce965cf140c609aa37778170f120d4992
ryw27 <rywbook06@gmail.com>
2025-07-26 20:19:33 -0400

    Admin delete registrations and update family balances

--- app/lib/semester/sem-actions.ts
+++ app/lib/semester/sem-actions.ts
@@ -775,6 +775,41 @@ export async function adminTransferStudent(regid: number, orgArrange: uiClasses,
     })
 }
 
+export async function adminDeleteRegistration(regid: number, curClass: uiClasses) {
+    await db.transaction(async (tx) => {
+        const reg = await tx.query.classregistration.findFirst({
+            where: (r, { eq }) => eq(r.regid, regid),
+            with: {
+                season: {}
+            }
+        });
+        if (!reg) {
+            throw new Error("Registration not found");
+        }
+
+        const orgBalance = await tx.query.familybalance.findFirst({
+            where: (fb, { and, eq }) => and(eq(fb.familyid, reg.familyid), eq(fb.seasonid, reg.seasonid))
+        });
+
+        if (!orgBalance) {
+            throw new Error("Original family balance corresponding to this registration was not found");
+        }
+
+        const orgTuition = reg.season.seasonid < reg.season.beginseasonid && reg.season.seasonid < reg.season.relatedseasonid 
+            ? Number(curClass.tuitionW) + Number(curClass.specialfeeW) + Number(curClass.bookfeeW) - (curClass.waiveregfee ? 0 : REGISTRATION_FEE) 
+            : Number(curClass.tuitionH) + Number(curClass.specialfeeH) + Number(curClass.bookfeeH) - (curClass.waiveregfee ? 0 : REGISTRATION_FEE);
+
+        await tx.update(familybalance).set({
+            tuition: (Number(orgBalance.tuition) - orgTuition).toString(),
+            totalamount: (Number(orgBalance.totalamount) - orgTuition).toString()
+        }).where(eq(familybalance.balanceid, orgBalance.balanceid));
+
+        await tx.delete(classregistration).where(eq(classregistration.regid, regid));
+        revalidatePath("/admintest/management/semester");
+        revalidatePath("/dashboard/classes");
+    })
+}
+
 export async function dropRegistration(regid: number, orgTuition: number) {
     await db.transaction(async (tx) => {
         const reg = await tx.query.classregistration.findFirst({
