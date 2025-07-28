ba10441c1fe20d75c73790e77cdd52ac57b09d20
ryw27 <rywbook06@gmail.com>
2025-07-28 17:15:53 -0400

    Deleted original sem folder

--- app/lib/semester/sem-utils.ts
+++
@@ -1,8 +0,0 @@
-import type { InferSelectModel } from "drizzle-orm";
-import { seasons } from "../db/schema";
-
-const SEMESTERONLY_SUITBALETERM_FOREIGNKEY = 2;
-
-export function inSpring(springSeason: InferSelectModel<typeof seasons>): boolean {
-    return new Date(Date.now()) >= new Date(springSeason.earlyregdate);
-} 
