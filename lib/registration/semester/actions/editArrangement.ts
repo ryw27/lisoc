6198d33cb6fbbc6c64534f5f2ce37ab08e3b7f25
ryw27 <rywbook06@gmail.com>
2025-07-29 23:56:25 -0400

    Properly edit reg class and its constituents

--- lib/registration/semester/actions/editArrangement.ts
+++ lib/registration/semester/actions/editArrangement.ts
@@ -1,87 +1,78 @@
 import { db } from "@/lib/db";
 import { arrangement } from "@/lib/db/schema";
 import { z } from "zod/v4";
-import { 
-    seasonObj, 
-    arrangementInsert 
-} from "../../types";
+import { seasonObj, arrangementInsert } from "@/lib/shared/types";
 import { eq } from "drizzle-orm";
 import { arrangementArraySchema } from "../../validation";
 import { arrangementSchema } from "@/lib/shared/validation";
-import { getTermVariables } from "../helpers";
+import { getTermVariables } from "../../helpers";
 import { toESTString } from "@/lib/utils";
 import { revalidatePath } from "next/cache";
 
 
-// TODO: Check this 
+// TODO: more efficiency? Not sure if this is updating all that have been edited or just all of them regardless
 export async function editArrangement(data: z.infer<typeof arrangementArraySchema>, season: seasonObj) {
     // const user = await requireRole(["ADMIN"]);
     return await db.transaction(async (tx) => {
         const parsedArray = arrangementArraySchema.parse(data);
         // Ensure arrangeid is present and valid. It should since it's an update
-        const { seasonid, activestatus, regstatus } = await getTermVariables(parsedArray.classrooms[0], season, tx);
+        const regClass = arrangementSchema.parse(parsedArray.classrooms[0])
+        if (!regClass.arrangeid || regClass.arrangeid === 0) {
+            throw new Error("Reg class identifier not found")
+        }
+
+        const { seasonid, activestatus, regstatus } = await getTermVariables(regClass, season, tx);
         const regClassObject = {
-            ...parsedArray.classrooms[0],
+            ...regClass,
             seasonid: seasonid,
             activestatus: activestatus,
             regstatus: regstatus,
-            tuitionW: parsedArray.classrooms[0].tuitionW?.toString() ?? null,
-            specialfeeW: parsedArray.classrooms[0].specialfeeW?.toString() ?? null,
-            bookfeeW: parsedArray.classrooms[0].bookfeeW?.toString() ?? null,
-            tuitionH: parsedArray.classrooms[0].tuitionH?.toString() ?? null,
-            specialfeeH: parsedArray.classrooms[0].specialfeeH?.toString() ?? null,
-            bookfeeH: parsedArray.classrooms[0].bookfeeH?.toString() ?? null,
+            tuitionW: regClass.tuitionW?.toString() ?? null,
+            specialfeeW: regClass.specialfeeW?.toString() ?? null,
+            bookfeeW: regClass.bookfeeW?.toString() ?? null,
+            tuitionH: regClass.tuitionH?.toString() ?? null,
+            specialfeeH: regClass.specialfeeH?.toString() ?? null,
+            bookfeeH: regClass.bookfeeH?.toString() ?? null,
             lastmodify: toESTString(new Date()),
             updateby: "testaccount"
         } satisfies arrangementInsert
 
-        for (const data of parsedArray.classrooms) {
-            const parsedData = arrangementSchema.parse(data);
-            if (parsedData.isregclass) {
-                if (typeof parsedData.arrangeid !== "number" || isNaN(parsedData.arrangeid)) {
-                    throw new Error("Update data does not contain a valid arrange ID identifier");
-                }
-                const { arrangeid, ...updateData } = regClassObject;
-                //const updated = 
+        await tx
+            .update(arrangement)
+            .set({
+                ...regClassObject
+            })
+            .where(eq(arrangement.arrangeid, regClass.arrangeid))
+
+        for (let i = 1; i < parsedArray.classrooms.length; i++) {
+            const parsedData = arrangementSchema.parse(parsedArray.classrooms[i]);
+            // Either update or insert
+            if (parsedData.arrangeid) {
+                // Updating
                 await tx
                     .update(arrangement)
                     .set({
-                        ...updateData
+                        ...regClassObject, // Handle changes to regclass
+                        isregclass: false, // In case
+                        classid: parsedData.classid, // Handle updates to unique cols for consituent classrooms
+                        teacherid: parsedData.teacherid,
+                        roomid: parsedData.roomid,
+                        seatlimit: parsedData.seatlimit
                     })
-                    .where(eq(arrangement.arrangeid, parsedData.arrangeid))
-                    .returning();
+                    .where(eq(arrangement.arrangeid, parsedData.arrangeid));
             } else {
-                // Either updating or adding
-                if (parsedData.arrangeid) {
-                    // Update
-                    const { arrangeid, ...updateData } = regClassObject;
-                    const updated = await tx
-                        .update(arrangement)
-                        .set({
-                            ...updateData, // Handles update to the three important columns for non reg classes
-                            isregclass: false,
-                            classid: parsedData.classid,
-                            teacherid: parsedData.teacherid,
-                            roomid: parsedData.roomid,
-                            seatlimit: parsedData.seatlimit
-                        })
-                        .where(eq(arrangement.arrangeid, parsedData.arrangeid));
-                } else {
-                    const { arrangeid, ...updateData } = regClassObject;
-                    await tx
-                        .insert(arrangement)
-                        .values({
-                            ...updateData,
-                            isregclass: false,
-                            classid: parsedData.classid,
-                            teacherid: parsedData.teacherid,
-                            roomid: parsedData.roomid,
-                            seatlimit: parsedData.seatlimit
-                        })
-                }
+                await tx
+                    .insert(arrangement)
+                    .values({
+                        ...regClassObject,
+                        isregclass: false,
+                        classid: parsedData.classid,
+                        teacherid: parsedData.teacherid,
+                        roomid: parsedData.roomid,
+                        seatlimit: parsedData.seatlimit
+                    })
             }
         }
-
         revalidatePath(`/admintest/management/semester`);
     })
 }
