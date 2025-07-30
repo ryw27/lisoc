c4fe4c7f6997234f030e7ba12e810be2d525a663
ryw27 <rywbook06@gmail.com>
2025-07-29 23:55:10 -0400

    Transfer logic for both admin and family

--- lib/registration/regchanges/actions/adminTransferStudent.ts
+++ lib/registration/regchanges/actions/adminTransferStudent.ts
@@ -2,53 +2,183 @@ import { db } from "@/lib/db";
 import { 
     classregistration, 
     familybalance,
-    arrangement
 } from "@/lib/db/schema";
 import { and, eq } from "drizzle-orm";
 import { uiClasses } from "@/lib/registration/types";
-import { REGISTRATION_FEE } from "@/lib/utils";
+import { 
+    EARLY_REG_DISCOUNT,
+    FAMILYBALANCE_STATUS_PENDING,
+    FAMILYBALANCE_STATUS_PROCESSED, 
+    FAMILYBALANCE_TYPE_PAYMENT, 
+    FAMILYBALANCE_TYPE_TRANSFER, 
+    LATE_REG_FEE_1, 
+    REGISTRATION_FEE, 
+    REGSTATUS_REGISTERED, 
+    REGSTATUS_SUBMITTED, 
+    REGSTATUS_TRANSFERRED, 
+    toESTString 
+} from "@/lib/utils";
+import { getArrSeason, getTotalPrice, isEarlyReg, isLateReg } from "../../helpers";
+import { famBalanceInsert } from "@/lib/shared/types";
 
 
-export async function adminTransferStudent(regid: number, orgArrange: uiClasses, newArrange: uiClasses) {
+export async function adminTransferStudent(regid: number, studentid: number, familyid: number, newArrange: uiClasses, override: boolean) {
+    // TODO: Parse data
     await db.transaction(async (tx) => {
-        const [newReg] = await tx
-            .update(classregistration)
-            .set({
-                arrangeid: newArrange.arrangeid as number,
-                classid: newArrange.classid
-            })
-            .where(eq(classregistration.regid, regid))
-            .returning()
-
-        // Update family balance
-        // TODO: Factor in whether it's a half or whole semester (Whole classes can be half registered )
-        let orgPrice = 0;
-        if (orgArrange.suitableterm === 2) {
-            orgPrice += Number(orgArrange.tuitionH) + Number(orgArrange.specialfeeH) + Number(orgArrange.bookfeeH) - (orgArrange.waiveregfee ? 0 : REGISTRATION_FEE);
-        } else {
-            orgPrice += Number(orgArrange.tuitionW) + Number(orgArrange.specialfeeW) + Number(orgArrange.bookfeeW) - (orgArrange.waiveregfee ? 0 : REGISTRATION_FEE);
-        }
-        let newPrice = 0;
-        if (newArrange.suitableterm === 2) {
-            newPrice += Number(newArrange.tuitionH) + Number(newArrange.specialfeeH) + Number(newArrange.bookfeeH) - (newArrange.waiveregfee ? 0 : REGISTRATION_FEE);
-        } else {
-            newPrice += Number(newArrange.tuitionW) + Number(newArrange.specialfeeW) + Number(newArrange.bookfeeW) - (newArrange.waiveregfee ? 0 : REGISTRATION_FEE);
-        }
-
-        const updated = await tx.query.familybalance.findFirst({
-            where: (fb, { and, eq }) => and(eq(fb.familyid, newReg.familyid), eq(fb.seasonid, newReg.seasonid))
+        // 1. Get old registration
+        const oldReg = await tx.query.classregistration.findFirst({
+            where: (cr, { eq }) => and(eq(cr.regid, regid), eq(cr.studentid, studentid))
         });
-
-        if (!updated) {
-            throw new Error("Family balance not found");
+        if (!oldReg) {
+            throw new Error("Did not find old class registration being transferred out of ");
         }
+
+        // 2. Check if already transferred or dropped out
+        if (oldReg.statusid !== REGSTATUS_SUBMITTED && oldReg.statusid !== REGSTATUS_REGISTERED) {
+            throw new Error("Invalid registration. Student has already transferred or dropped this class");
+        }
+
+        // 3. Find the old arrangement to check cancel deadline
+        // Having the arrangeid check is nice, but it's not technically postgres level enforced to be an actual arrangement, which is why there is another check
+        const oldArr = await tx.query.arrangement.findFirst({
+            where: (arr, { and, or, eq }) => or(
+                eq(arr.arrangeid, oldReg.arrangeid),
+                and(
+                    eq(arr.classid, oldReg.classid),
+                    eq(arr.seasonid, oldReg.seasonid)
+                )
+            ),
+            with: {
+                season: {
+                    columns: {
+                        canceldeadline: true
+                    }
+                }
+            }
+        })
+        if (!oldArr) {
+            throw new Error("Cannot find original class in transfer.");
+        }
+
+        // 4. Check if it's past cancel deadline and there is no admin override
+        if (new Date(oldArr.season.canceldeadline) <= new Date(toESTString(new Date())) && !override) {
+            throw new Error("Transfer is not allowed. Cancel deadline has passed");
+        }
+
+        // 5. Check if they haven't paid yet. In this case just delete the old registration
+        if (oldReg.statusid === REGSTATUS_SUBMITTED) {
+            await tx
+                .delete(classregistration)
+                .where(eq(classregistration.regid, oldReg.regid));
+        } else {
+            // 6. Old reg has been paid: set it to transferred
+            await tx
+                .update(classregistration)
+                .set({
+                    statusid: REGSTATUS_TRANSFERRED,
+                })
+                .where(and(eq(classregistration.regid, regid), eq(classregistration.studentid, studentid)))
+                .returning()
+        }
+
+        // 6. Take away the tuition from this class by inserting a new family balance row
+        const oldTotalPrice = await getTotalPrice(tx, oldArr);
+        // Made these outside of the insert because there's some weird typescript error which i'm pretty sure is a drizzle bug
+        // TODO: Check these values to ensure the price calculations are correct. Remove reg fee? What do you do about early discounts? etc.
+        const removeFamBalValues = {
+            appliedregid: oldReg.regid,
+            appliedid: oldReg.familybalanceid || 0, // TODO: Not postgres enforced. 
+            seasonid: oldArr.seasonid,
+            familyid: familyid,
+            regfee: (oldArr.waiveregfee ? 0 : -REGISTRATION_FEE).toString(),
+            tuition: (-oldTotalPrice).toString(),
+            totalamount: ((oldArr.waiveregfee ? 0 : -REGISTRATION_FEE) - oldTotalPrice).toString(),
+            typeid: FAMILYBALANCE_TYPE_TRANSFER,
+            statusid: FAMILYBALANCE_STATUS_PENDING,
+            notes: "Admin transfer student out, subtract old class fees"
+        } satisfies famBalanceInsert
+
+        const [removeOldPrice] = await tx
+            .insert(familybalance)
+            .values(removeFamBalValues)
+            .returning();
+
+        // 7. If the original class was already paid for, pay the family back since we just removed the old balance
+        // Old reg was queried before all updates
+        if (oldReg.statusid === REGSTATUS_REGISTERED) { 
+            // Insert paypal/official transaction here
+            const payFamValues = {
+                appliedregid: oldReg.regid,
+                appliedid: removeOldPrice.balanceid,
+                seasonid: oldArr.seasonid,
+                familyid: familyid,
+                regfee: (oldArr.waiveregfee ? REGISTRATION_FEE : 0).toString(),
+                // earlyregdiscount: probably want to take this back, but how?
+                // lateregfee: same with this value
+                tuition: oldTotalPrice.toString(),
+                totalamount:((oldArr.waiveregfee ? 0 : REGISTRATION_FEE) + oldTotalPrice).toString(),
+                typeid: FAMILYBALANCE_TYPE_PAYMENT, // TODO: Check this
+                statusid: FAMILYBALANCE_STATUS_PROCESSED,
+                notes: "Admin transfer student out, refund family reg fee"
+            } satisfies famBalanceInsert
+
+            await tx
+                .insert(familybalance)
+                .values(payFamValues);
+        }
+
+        // 8. Insert new class registration
+        const arrTerm = await getArrSeason(tx, newArrange);
+        const now = toESTString(new Date());
+        const [newReg] = await tx
+            .insert(classregistration)
+            .values({
+                appliedid: oldReg.regid,
+                studentid: studentid,
+                arrangeid: newArrange.arrangeid,
+                seasonid: newArrange.seasonid,
+                isyearclass: arrTerm === "year",
+                classid: newArrange.classid,
+                registerdate: now,
+                familyid: familyid,
+                byadmin: true,
+                notes: `Admin transfer student to ${newArrange.arrangeid} from ${oldReg.arrangeid}`
+            })
+            .returning()
+        // 9. Insert new family balance with registration prices
+        // TODO: Another field to consider is extrafee4newfamily
+        const newTotalPrice = await getTotalPrice(tx, newArrange, arrTerm);
+        const regFee = newArrange.waiveregfee ? 0 : REGISTRATION_FEE ;
+        const earlyregdiscount = (await isEarlyReg(tx, newArrange)) ? EARLY_REG_DISCOUNT : 0;
+        const lateregfee = (await isLateReg(tx, newArrange)) ? LATE_REG_FEE_1 : 0;
+        const totalamount =  newTotalPrice + regFee + lateregfee - earlyregdiscount;
+
+        const newBalVals = {
+            appliedid: oldReg.familybalanceid || 0,
+            appliedregid: newReg.regid,
+            seasonid: newArrange.seasonid,
+            familyid: familyid,
+            regfee: regFee.toString(),
+            earlyregdiscount: earlyregdiscount.toString(),
+            lateregfee: lateregfee.toString(),
+            tuition: (newTotalPrice).toString(),
+            totalamount: (totalamount).toString(),
+            typeid: FAMILYBALANCE_TYPE_TRANSFER,
+            registerdate: now,
+            notes: "Admin transferred student, new balance" 
+        } satisfies famBalanceInsert
+
+        const [newRegBal] = await tx
+            .insert(familybalance)
+            .values(newBalVals)
+            .returning();
 
         await tx
-            .update(familybalance)
+            .update(classregistration)
             .set({
-                tuition: (Number(updated.tuition) - orgPrice + newPrice).toString(),
-                totalamount: (Number(updated.totalamount) - orgPrice + newPrice).toString()
+                familybalanceid: newRegBal.balanceid
             })
-            .where(and(eq(familybalance.familyid, newReg.familyid), eq(familybalance.seasonid, newReg.seasonid)));
+
+        return newRegBal;
     })
 }
--- lib/registration/regchanges/actions/familyRequestTransfer.ts
+++ lib/registration/regchanges/actions/familyRequestTransfer.ts
@@ -1,48 +1,185 @@
 import { db } from "@/lib/db";
+import {  classregistration, familybalance } from "@/lib/db/schema";
+import { and, eq } from "drizzle-orm";
 import { 
-    classregistration, 
-    regchangerequest,
-} from "@/lib/db/schema";
-import { eq } from "drizzle-orm";
-import { toESTString } from "@/lib/utils";
+    EARLY_REG_DISCOUNT,
+    FAMILYBALANCE_STATUS_PENDING,
+    FAMILYBALANCE_STATUS_PROCESSED, 
+    FAMILYBALANCE_TYPE_PAYMENT, 
+    FAMILYBALANCE_TYPE_TRANSFER, 
+    LATE_REG_FEE_1, 
+    REGISTRATION_FEE, 
+    REGSTATUS_REGISTERED, 
+    REGSTATUS_SUBMITTED, 
+    REGSTATUS_TRANSFERRED, 
+    toESTString 
+} from "@/lib/utils";
 import { revalidatePath } from "next/cache";
+import { uiClasses } from "../../types";
+import { getArrSeason, getTotalPrice, isEarlyReg, isLateReg } from "../../helpers";
+import { famBalanceInsert } from "@/lib/shared/types";
+// import { requireRole } from "@/lib/auth";
 
 
 // TODO: Ensure that this is a valid transfer, that the family has paid for the original class. Enforced on client at the current moment.
-export async function familyRequestTransfer(regid: number) {
+export async function familyRequestTransfer(regid: number, studentid: number, familyid: number, newArrange: uiClasses) {
+    // TODO: Parse data
+    // const user = await requireRole(["FAMILY"]);
     await db.transaction(async (tx) => {
-        const regObj = await tx.query.classregistration.findFirst({
-            where: (cr, { eq }) => eq(cr.regid, regid)
+        // 1. Get old registration
+        const oldReg = await tx.query.classregistration.findFirst({
+            where: (cr, { and, eq }) => and(eq(cr.regid, regid), eq(cr.studentid, studentid))
         });
-        if (!regObj) {
-            throw new Error("Cannot find given registration");
+        if (!oldReg) {
+            throw new Error("Did not find old class registration being transferred out of ");
         }
+
+        // 2. Check if already transferred or dropped out
+        if (oldReg.statusid !== REGSTATUS_SUBMITTED && oldReg.statusid !== REGSTATUS_REGISTERED) {
+            throw new Error("Invalid registration. Student has already transferred or dropped this class");
+        }
+
+        // 3. Find the old arrangement to check cancel deadline
+        // Having the arrangeid check is nice, but it's not technically postgres level enforced to be an actual arrangement, which is why there is another check
+        const oldArr = await tx.query.arrangement.findFirst({
+            where: (arr, { and, or, eq }) => or(
+                eq(arr.arrangeid, oldReg.arrangeid),
+                and(
+                    eq(arr.classid, oldReg.classid),
+                    eq(arr.seasonid, oldReg.seasonid)
+                )
+            ),
+            with: {
+                season: {
+                    columns: {
+                        canceldeadline: true
+                    }
+                }
+            }
+        })
+        if (!oldArr) {
+            throw new Error("Cannot find original class in transfer.");
+        }
+
+        // 4. Check if it's past cancel deadline and there is no admin override
+        if (new Date(oldArr.season.canceldeadline) <= new Date(toESTString(new Date()))) {
+            throw new Error("Transfer is not allowed. Cancel deadline has passed");
+        }
+
+        // 5. Check if they haven't paid yet. In this case just delete the old registration
+        if (oldReg.statusid === REGSTATUS_SUBMITTED) {
+            await tx
+                .delete(classregistration)
+                .where(eq(classregistration.regid, oldReg.regid));
+        } else {
+            // 6. Old reg has been paid: set it to transferred
+            await tx
+                .update(classregistration)
+                .set({
+                    statusid: REGSTATUS_TRANSFERRED,
+                })
+                .where(and(eq(classregistration.regid, regid), eq(classregistration.studentid, studentid)))
+                .returning()
+        }
+
+        // 6. Take away the tuition from this class by inserting a new family balance row
+        const oldTotalPrice = await getTotalPrice(tx, oldArr);
+        // Made these outside of the insert because there's some weird typescript error which i'm pretty sure is a drizzle bug
+        // TODO: Check these values to ensure the price calculations are correct. Remove reg fee? What do you do about early discounts? etc.
+        const removeFamBalValues = {
+            appliedregid: oldReg.regid,
+            appliedid: oldReg.familybalanceid || 0, // TODO: Not postgres enforced. 
+            seasonid: oldArr.seasonid,
+            familyid: familyid,
+            regfee: (oldArr.waiveregfee ? 0 : -REGISTRATION_FEE).toString(),
+            tuition: (-oldTotalPrice).toString(),
+            totalamount: ((oldArr.waiveregfee ? 0 : -REGISTRATION_FEE) - oldTotalPrice).toString(),
+            typeid: FAMILYBALANCE_TYPE_TRANSFER,
+            statusid: FAMILYBALANCE_STATUS_PENDING,
+            notes: "Admin transfer student out, subtract old class fees"
+        } satisfies famBalanceInsert
+
+        const [removeOldPrice] = await tx
+            .insert(familybalance)
+            .values(removeFamBalValues)
+            .returning();
+
+        // 7. If the original class was already paid for, pay the family back since we just removed the old balance
+        // Old reg was queried before all updates
+        if (oldReg.statusid === REGSTATUS_REGISTERED) { 
+            // Insert paypal/official transaction here
+            const payFamValues = {
+                appliedregid: oldReg.regid,
+                appliedid: removeOldPrice.balanceid,
+                seasonid: oldArr.seasonid,
+                familyid: familyid,
+                regfee: (oldArr.waiveregfee ? REGISTRATION_FEE : 0).toString(),
+                // earlyregdiscount: probably want to take this back, but how?
+                // lateregfee: same with this value
+                tuition: oldTotalPrice.toString(),
+                totalamount:((oldArr.waiveregfee ? 0 : REGISTRATION_FEE) + oldTotalPrice).toString(),
+                typeid: FAMILYBALANCE_TYPE_PAYMENT, // TODO: Check this
+                statusid: FAMILYBALANCE_STATUS_PROCESSED,
+                notes: "Admin transfer student out, refund family reg fee"
+            } satisfies famBalanceInsert
+
+            await tx
+                .insert(familybalance)
+                .values(payFamValues);
+        }
+
+        // 8. Insert new class registration
+        const arrTerm = await getArrSeason(tx, newArrange);
+        const now = toESTString(new Date());
+        const [newReg] = await tx
+            .insert(classregistration)
+            .values({
+                appliedid: oldReg.regid,
+                studentid: studentid,
+                arrangeid: newArrange.arrangeid,
+                seasonid: newArrange.seasonid,
+                isyearclass: arrTerm === "year",
+                classid: newArrange.classid,
+                registerdate: now,
+                familyid: familyid,
+                byadmin: true,
+                notes: `Admin transfer student to ${newArrange.arrangeid} from ${oldReg.arrangeid}`
+            })
+            .returning()
+        // 9. Insert new family balance with registration prices
+        // TODO: Another field to consider is extrafee4newfamily
+        const newTotalPrice = await getTotalPrice(tx, newArrange, arrTerm);
+        const regFee = newArrange.waiveregfee ? 0 : REGISTRATION_FEE ;
+        const earlyregdiscount = (await isEarlyReg(tx, newArrange)) ? EARLY_REG_DISCOUNT : 0;
+        const lateregfee = (await isLateReg(tx, newArrange)) ? LATE_REG_FEE_1 : 0;
+        const totalamount =  newTotalPrice + regFee + lateregfee - earlyregdiscount;
+
+        const newBalVals = {
+            appliedid: oldReg.familybalanceid || 0,
+            appliedregid: newReg.regid,
+            seasonid: newArrange.seasonid,
+            familyid: familyid,
+            regfee: regFee.toString(),
+            earlyregdiscount: earlyregdiscount.toString(),
+            lateregfee: lateregfee.toString(),
+            tuition: (newTotalPrice).toString(),
+            totalamount: (totalamount).toString(),
+            typeid: FAMILYBALANCE_TYPE_TRANSFER,
+            registerdate: now,
+            notes: "Admin transferred student, new balance" 
+        } satisfies famBalanceInsert
+
+        const [newRegBal] = await tx
+            .insert(familybalance)
+            .values(newBalVals)
+            .returning();
 
         await tx
             .update(classregistration)
             .set({
-                previousstatusid: regObj.statusid, 
+                familybalanceid: newRegBal.balanceid
             })
-            .where(eq(classregistration.regid, regid))
-        await tx
-            .insert(regchangerequest)
-            .values({
-                regid: regid,
-                appliedid: regid,
-                studentid: regObj.studentid,
-                seasonid: regObj.seasonid,
-                isyearclass: regObj.isyearclass,
-                classid: regObj.classid,
-                registerdate: regObj.registerdate,
-                oriregstatusid: regObj.statusid,
-                regstatusid: regObj.statusid,
-                reqstatusid: 1, // Pending
-                familybalanceid: regObj.familybalanceid,
-                familyid: regObj.familyid,
-                submitdate: toESTString(new Date()),
-                notes: "Transfer Request",
-            })
-        revalidatePath("/admintest/management/semester");
-        revalidatePath("/dashboard/register");
+
+        return newRegBal;
     })
 }
