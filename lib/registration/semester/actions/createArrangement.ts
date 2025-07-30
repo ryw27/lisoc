bd6e1812f8b87476ba215b0d9317c81c3fbf2e1e
ryw27 <rywbook06@gmail.com>
2025-07-29 23:55:58 -0400

    Create reg  class with constituents

--- lib/registration/semester/actions/createArrangement.ts
+++ lib/registration/semester/actions/createArrangement.ts
@@ -1,56 +1,78 @@
 import { db } from "@/lib/db";
 import { arrangement } from "@/lib/db/schema";
 import { z } from "zod/v4";
-import { seasonObj } from "../../types";
+import { seasonObj } from "@/lib/shared/types";
 import { arrangementArraySchema } from "../../validation";
 import { arrangementSchema } from "@/lib/shared/validation";
-import { getTermVariables } from "../helpers";
-import { toESTString } from "@/lib/utils";
+import { getTermVariables } from "../../helpers";
+import { toESTString, UNKNOWN_CLASSROOMID, UNKNOWN_TEACHERID } from "@/lib/utils";
 import { revalidatePath } from "next/cache";
 
-
+// Create new regclas with constituent classrooms
 export async function createArrangement(data: z.infer<typeof arrangementArraySchema>, season: seasonObj) {
-    // TODO: Auth check 
+    // 1. Auth Check 
     // const user = await requireRole(["ADMIN"]);
 
-    // Parse data
+    // 2. Parse data
     const parsedArray = arrangementArraySchema.parse(data);
-
+    
+    // 3. Begin transaction
     return await db.transaction(async (tx) => {
-        for (const data of parsedArray.classrooms) {
-            const parsedData = arrangementSchema.parse(data);
-            const { seasonid, activestatus, regstatus } = await getTermVariables(parsedData, season, tx);
-            console.log(parsedData.roomid);
+        // 5. Get reg class values
+        const regclass = arrangementSchema.parse(parsedArray.classrooms[0])
+        const { seasonid, activestatus, regstatus } = await getTermVariables(regclass, season, tx);
+        const regClassValues = {
+            seasonid: seasonid,
+            classid: regclass.classid,
+            teacherid: UNKNOWN_TEACHERID,
+            roomid: UNKNOWN_CLASSROOMID,
+            timeid: regclass.timeid,
+            agelimit: regclass.agelimit,
+            suitableterm: regclass.suitableterm,
+            waiveregfee: regclass.waiveregfee,
+            activestatus: activestatus,
+            regstatus: regstatus,
+            closeregistration: regclass.closeregistration,
+            tuitionW: regclass.tuitionW.toString(),
+            bookfeeW: regclass.bookfeeW.toString(),
+            specialfeeW: regclass.specialfeeW.toString(),
+            tuitionH: regclass.tuitionH.toString(),
+            bookfeeH: regclass.bookfeeH.toString(),
+            specialfeeH: regclass.specialfeeH.toString(),
+            notes: regclass.notes ?? "",
+            lastmodify: toESTString(new Date()),
+            isregclass: true,
+            updateby: "admin"   // TODO: Switch to user
+        }
+        // 6. Insert reg class
+        await tx
+            .insert(arrangement)
+            .values({
+                ...regClassValues,
+            })
 
+        // 7. Loop through constituent classrooms
+        for (let i = 1; i < parsedArray.classrooms.length; i++) {
+            // 8. Parse 
+            const parsedData = arrangementSchema.parse(parsedArray.classrooms[i]); 
+            // 9. Insert
+            // Remember that the only unique fields of classrooms are classid, teacherid, roomid, seatlimit, and notes
             const inserted = await tx
                 .insert(arrangement)
                 .values({
-                    seasonid: seasonid,
+                    ...regClassValues,
                     classid: parsedData.classid,
                     teacherid: parsedData.teacherid,
                     roomid: parsedData.roomid,
-                    timeid: parsedData.timeid,
                     seatlimit: parsedData.seatlimit,
-                    agelimit: parsedData.agelimit,
-                    suitableterm: parsedData.suitableterm,
-                    waiveregfee: parsedData.waiveregfee,
-                    activestatus: activestatus,
-                    regstatus: regstatus,
-                    closeregistration: parsedData.closeregistration,
-                    tuitionW: parsedData.tuitionW.toString(),
-                    bookfeeW: parsedData.bookfeeW.toString(),
-                    specialfeeW: parsedData.specialfeeW.toString(),
-                    tuitionH: parsedData.tuitionH.toString(),
-                    bookfeeH: parsedData.bookfeeH.toString(),
-                    specialfeeH: parsedData.specialfeeH.toString(),
                     notes: parsedData.notes ?? "",
-                    lastmodify: toESTString(new Date()),
-                    isregclass: true,
-                    updateby: "admin"
+                    isregclass: false,
                 })
                 .returning();
-            return inserted[0];
         }
+
+        // 10. Revalidate
         revalidatePath(`/admintest/management/semester`);
+        revalidatePath("/dashboard/register");
     })
 }
