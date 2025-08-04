ea854184796d64a370288165b7dd38c2a74b5815
ryw27 <rywbook06@gmail.com>
2025-08-03 22:09:26 -0400

    Improved createStudent

--- lib/family/actions/createStudent.ts
+++ lib/family/actions/createStudent.ts
@@ -4,23 +4,49 @@ import { student } from "@/lib/db/schema";
 import { z } from "zod/v4";
 import { toESTString } from "@/lib/utils";
 import { studentSchema } from "../validation";
+import { requireRole } from "@/lib/auth";
+import { InferInsertModel } from "drizzle-orm";
 
 
 export async function createStudent(data: z.infer<typeof studentSchema>, familyid: number) {
     const parsed = studentSchema.parse(data);
+    await requireRole(["FAMILY"]);
 
-    // Check user role
-    // const user = await requireRole(["FAMILY"], { redirect: false });
-    // if (!user || new Date(user.expires) >= new Date(Date.now())) {
-    //     throw new Error("Expired user session. Please login again");
-    // }
     return await db.transaction(async (tx) => {
-        const [inserted] = await tx.insert(student).values({
-            ...parsed,
+        const family = await tx.query.family.findFirst({
+            where: (family, { eq }) => eq(family.familyid, familyid)
+        });
+        if (!family) {
+            throw new Error("Family not found");
+        }
+
+        const curStudents = await tx.query.student.findMany({
+            where: (student, { eq }) => eq(student.familyid, family.familyid)
+        });
+
+        const newStudentNo = curStudents.length + 1;
+
+        const newStudent = {
             familyid: familyid,
+            studentno: newStudentNo.toString(),
+            namecn: parsed.namecn ?? "",
+            namelasten: parsed.namelasten ?? "",
+            namefirsten: parsed.namefirsten ?? "",
+            gender: parsed.gender ?? "",
+            ageof: parsed.age.toString(),
+            age: parsed.age,
+            dob: toESTString(parsed.dob),
+            active: parsed.active,
             createddate: toESTString(new Date()),
             lastmodify: toESTString(new Date()),
-        }).returning();
+            notes: parsed.notes ?? "",
+            upgradable: 0,
+        } satisfies InferInsertModel<typeof student>;
+
+        const [inserted] = await tx
+            .insert(student)
+            .values(newStudent)
+            .returning();
         return inserted;
     })
 }
