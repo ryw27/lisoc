2564456c73d3bd25d0b1b6ac12e894ad17b9c914
ryw27 <rywbook06@gmail.com>
2025-08-10 23:30:13 -0400

    UI components for ease

---
+++ components/data-view/entity-forms-header.tsx
@@ -0,0 +1,33 @@
+import { ArrowLeft, Edit } from "lucide-react"
+import Link from "next/link"
+
+
+type entityFormsHeaderProps = {
+    type: "add" | "edit" | "view"
+    gobacklink: string;
+    editlink?: string;
+}
+export default function EntityFormsHeader({ 
+    type,
+    gobacklink,
+    editlink,
+}: entityFormsHeaderProps) {
+    return (
+        <div className="container mx-auto max-w-5xl flex justify-between">
+            <Link
+                className="flex items-center gap-1 text-blue-600 underline text-xs font-bold"
+                href={gobacklink}
+            >
+                <ArrowLeft className="w-4 h-4" /> Go Back
+            </Link>
+            {/* {type === "view" && (
+                <Link
+                    className="flex items-center gap-1 text-blue-600 underline text-xs font-bold"
+                    href={editlink as string}
+                >
+                    Edit <Edit className="w-4 h-4" /> 
+                </Link>
+            )} */}
+        </div>
+    ) 
+}
