b9c2ae5669cf9b8a6e10d8a7f51ec4536c3dc6cb
ryw27 <rywbook06@gmail.com>
2025-08-12 15:20:06 -0400

    Use session information on home page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,10 +2,11 @@ import { requireRole } from "@/lib/auth";
 
 export default async function TestPage() {
     const session = await requireRole(["ADMIN"]);
-    console.log(session);
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold text-center">Hello user</h1>
+            <h1 className="text-2xl font-bold text-center">Hello {session.user.name}</h1>
+            <p className="text-lg text-center">Welcome to the admin dashboard</p>
+            <p className="text-lg text-center">You are logged in as {session.user.email}</p>
         </div>
     )
 }
