b4e90274a3a6cc1677c1911dad3993d95205b59e
ryw27 <rywbook06@gmail.com>
2025-08-15 16:32:37 -0400

    Insert Row updated to account for the double insert required for adminuser and teacher. Admin cannot make families, not included

--- lib/data-view/actions/insertRow.ts
+++ lib/data-view/actions/insertRow.ts
@@ -1,12 +1,67 @@
 "use server";
 import { db } from "@/lib/db";
-import { z } from "zod/v4";
+import { z }  from "zod/v4";
 import { requireRole } from "@/lib/auth";
 import { revalidatePath } from "next/cache";
 import { ADMIN_DATAVIEW_LINK } from "@/lib/utils";
-import { classes } from "@/lib/db/schema";
+import { classes, users } from "@/lib/db/schema";
 import { getEntityConfig, Registry } from "@/lib/data-view/registry";
 import { eq, InferInsertModel } from "drizzle-orm";
+import { Transaction } from "@/lib/registration/helpers";
+import { AdminSchema, AdminUserJoined } from "../entity-configs/(people)/adminuser";
+import { TeacherJoined, TeacherSchema } from "../entity-configs/(people)/teacher";
+import { FamilyJoined } from "../entity-configs/(people)/family";
+import bcrypt from "bcrypt";
+import { sendUserRegEmail } from "../helpers";
+
+
+const UserInsertTables = ["adminuser", "teacher"];
+async function insertUser(tx: Transaction, data: AdminUserJoined | TeacherJoined | FamilyJoined, entity: keyof Registry) {
+    // Determine role based on entity type
+    const roleMap = {
+        adminuser: "ADMINUSER" as const,
+        teacher: "TEACHER" as const, 
+    };
+
+    const exists = await tx.query.users.findFirst({
+        where: (u, { eq }) => eq(u.email, data.email)
+    });
+
+    if (exists) {
+        const newRole = roleMap[entity as keyof typeof roleMap];
+        if (exists.roles.includes(newRole)) {
+            throw new Error(`User with ${data.email} already has this role ${entity}`);
+        }
+        const updatedRoles = [...exists.roles, newRole];
+        const [user] = await tx
+            .update(users)
+            .set({
+                roles: updatedRoles
+            })
+            .where(eq(users.id, exists.id))
+            .returning();
+        const newUser = false 
+        return { user, newUser };
+    }
+    
+    const [user] = await tx
+        .insert(users)
+        .values({
+            name: data.name,
+            email: data.email,
+            password: await bcrypt.hash(data.password, 10),
+            roles: [roleMap[entity as keyof typeof roleMap]],
+            address: data.address,
+            city: data.city, 
+            state: data.state,
+            zip: data.zip, 
+            phone: data.phone,
+        })
+        .returning()
+    const newUser = true; 
+    return { user, newUser };
+}
+
 
 export interface ActionResult {
     ok: boolean;
@@ -42,21 +97,59 @@ export async function insertRow(
             ...insertExtras
         } as InferInsertModel<typeof table>;
 
-        const [row] = await db
-            // drizzle generic table typing is too strict here; runtime is correct
-            // @ts-ignore
-            .insert(table)
-            .values(insertData)
-            .returning();
+        const row = await db.transaction(async (tx) => {
+            // For user-related entities, insert user first
+            if (UserInsertTables.includes(entity)) {
+                const { user, newUser } = await insertUser(tx, parsed.data as unknown as AdminUserJoined | TeacherJoined, entity);
+                const schemaMap = {
+                    "adminuser": AdminSchema,
+                    "teacher": TeacherSchema,
+                } as const;
+
+                const schema = schemaMap[entity as keyof typeof schemaMap];
+                const result = schema.strip().parse(parsed.data); // Strip the users fields
+                // if (result.error) {
+                //     const flat = z.flattenError(result.error);
+                //     return {
+                //         ok: false,
+                //         message: "Validation failed. Please correct the highlighted fields.",
+                //         fieldErrors: flat.fieldErrors as Record<string, string[]>,
+                //         formErrors: flat.formErrors,
+                //     }; 
+                // }
+
+                const specificData = result;
+                // Add user ID to insert data
+                const userInsertData = {
+                    ...specificData,
+                    userid: user.id,
+                    ischangepwdnext: newUser ? true : false,
+                } as InferInsertModel<typeof table>;
+                
+                const [entityRow] = await tx
+                    .insert(table)
+                    .values(userInsertData)
+                    .returning();
+
+                await sendUserRegEmail(parsed.data.email as string, parsed.data.password as string, entity === "adminuser" ? "Admin" : "Teacher");
+                return entityRow
+            } else {
+                const [entityRow] = await tx
+                    .insert(table)
+                    .values(insertData)
+                    .returning();
+                return entityRow
+            }
+        });
 
         if (!row) {
-            console.error(`Insert failed for ${table._.name} with row ${row}`);
+            console.error(`Insert failed for ${entity} with row ${row}`);
             return { ok: false, message: "Unknown database error occurred" };
         }
 
         // Exception placed for self referential columns, only for classes
         const itself = primaryKey === "classid";
-        if (itself && primaryKey === "classid") {
+        if (itself) {
             await db
                 .update(classes)
                 .set({
