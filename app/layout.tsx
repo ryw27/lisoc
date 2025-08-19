56161abf2813c8f2bc72653ee72c984069afeb43
ryw27 <rywbook06@gmail.com>
2025-08-18 23:41:58 -0400

    next-intl client provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,43 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { getLocale } from "next-intl/server";
+import { NextIntlClientProvider } from 'next-intl';
+
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "LISOC",
-  description: "LISOC Website",
-  icons: {
-    icon: '/lisoc.png',
-  },
+	title: "LISOC 华夏中文学校",
+	description: "LISOC Website",
+	icons: {
+		icon: '/lisoc.png',
+	},
 };
 
-export default function RootLayout({
-  children,
+export default async function RootLayout({
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	const locale = await getLocale();
+	return (
+		<html lang={locale}>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				<NextIntlClientProvider>
+					{children}
+				</NextIntlClientProvider>
+			</body>
+		</html>
+	);
 }
