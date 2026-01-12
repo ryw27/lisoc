3752ffcdb47c20eb8c3a332c82c81f1261898a44
ryw27 <rywbook06@gmail.com>
2026-01-12 16:07:48 -0500

    put page for data seasons link

---
+++ app/admin/data/(semester-pages)/seasons/page.tsx
@@ -0,0 +1,15 @@
+export default async function SemestersPage() {
+    // const semesters = await db
+    //     .select()
+    //     .from(seasons)
+    //     .where(eq(seasons.status, "Active"))
+    //     .limit(1)
+    //     .execute();
+
+    return (
+        <div>
+            <h1>Semesters</h1>
+            <pre>In Progress</pre>
+        </div>
+    );
+}
